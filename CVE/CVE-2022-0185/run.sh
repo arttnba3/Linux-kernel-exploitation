eaac593043c269d63156edebe20b975854c7f5cb
arttnba3 <arttnba3@outlook.com>
2023-05-27 18:49:58 -0700

    add CVE-2022-0185 exp, pack exp binary in filesystem

---
+++ CVE/CVE-2022-0185/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+stty intr ^]
+cd `dirname $0`
+qemu-system-x86_64 \
+	-m 256M \
+	-cpu kvm64,+smep,+smap \
+    -smp cores=2,threads=2 \
+	-kernel bzImage \
+	-initrd ./rootfs.cpio \
+    -nographic \
+	-monitor /dev/null \
+	-snapshot \
+	-append "console=ttyS0 kaslr pti=on quiet oops=panic panic=1" \
+	-no-reboot \
+    -s
